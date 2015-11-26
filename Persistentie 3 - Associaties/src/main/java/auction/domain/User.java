706a59cbbafb7b702c6546e52ca280bf97f58aa9
JWiddershoven <jellewiddershoven@gmail.com>
2015-11-26 17:54:19 +0100

    Persistentie 3 ininitial

---
+++ Persistentie 3 - Associaties/src/main/java/auction/domain/User.java
@@ -0,0 +1,28 @@
+package auction.domain;
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+@NamedQueries({
+    @NamedQuery(name = "User.getAll", query = "select u from User as u"),
+    @NamedQuery(name = "User.count", query = "select count(u) from User as u"),
+    @NamedQuery(name = "User.findByEmail", query = "select u from User as u where u.email = :userEmail")
+})
+public class User implements Serializable {
+
+    @Id
+    private String email;
+
+    public User() {
+        
+    }
+    
+    public User(String email) {
+        this.email = email;
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
