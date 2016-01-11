140c2c51ccd60b1a5e66cb3e755cb6cdcd6787c6
Jelle Widdershoven <jellewiddershoven@gmail.com>
2016-01-11 14:54:17 +0100

    SignFile is af

--- DigitaleHandtekening/src/digitalehandtekening/SignFile.java
+++ DigitaleHandtekening/src/digitalehandtekening/SignFile.java
@@ -5,22 +5,26 @@
  */
 package digitalehandtekening;
 
-import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.Signature;
 import java.security.SignatureException;
+import java.security.interfaces.RSAPrivateKey;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JFileChooser;
@@ -74,24 +78,42 @@ public class SignFile {
         }
         
         FileInputStream fis = new FileInputStream(privateKey);
-        DataInputStream dis = new DataInputStream(fis);
-        byte[] privateKeyBytes = new byte[(int) privateKey.length()];
-        dis.readFully(privateKeyBytes);
-        dis.close();
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        PrivateKey pKey = null;
+        try {
+            pKey = (PrivateKey) ois.readObject();
+        }
+        catch (ClassNotFoundException ex) {
+            Logger.getLogger(SignFile.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        ois.close();
         
         Signature sig = Signature.getInstance("SHA1withRSA");
-        KeyFactory kf = KeyFactory.getInstance("RSA");
-        PrivateKey pKey = kf.generatePrivate(new PKCS8EncodedKeySpec(privateKeyBytes)); 
-        sig.initSign(pKey);
+        
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+
+        PKCS8EncodedKeySpec privSpec = new PKCS8EncodedKeySpec(pKey.getEncoded());
+        RSAPrivateKey privKey = (RSAPrivateKey) keyFactory.generatePrivate(privSpec);
+        
+        sig.initSign(privKey);
         
         FileInputStream fis2 = new FileInputStream(inputEXT);
         DataInputStream dis2 = new DataInputStream(fis2);
         byte[] inputEXTBytes = new byte[(int) inputEXT.length()];
-        dis.readFully(inputEXTBytes);
-        dis.close();
+        dis2.readFully(inputEXTBytes);
+        dis2.close();
         
         sig.update(inputEXTBytes);
-        String signedMessage = new String(Base64.encode(sig.sign()));
-        System.out.println(signedMessage);
+        byte[] signature = sig.sign();
+        Byte length = (byte) signature.length;
+        //int length2 = Byte.toUnsignedInt(length);
+        
+        DataOutputStream dos = new DataOutputStream(new FileOutputStream(inputEXT.getPath().substring(0, inputEXT.getPath().lastIndexOf(File.separator)) + File.separator + "INPUT(SignedBy" + naam + ").EXT"));
+        dos.write(length);
+        dos.write(signature);
+        dos.write(inputEXTBytes);
+    
+        dos.close();
     } 
 }
